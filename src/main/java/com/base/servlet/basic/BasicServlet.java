c22226512f83495d56b2216129c8f1a05b412a9e
chris1797 <jaehun6004@gmail.com>
2023-12-19 00:09:16 +0900

    BasicServlet setup

--- src/main/java/com/base/servlet/basic/BasicServlet.java
+++ src/main/java/com/base/servlet/basic/BasicServlet.java
@@ -6,14 +6,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 @WebServlet(name = "basicServlet", urlPatterns = "/api")
 public class BasicServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         System.out.println("Start BasicServlet :::");
+        System.out.println("request ::: " + request); // org.apache.catalina.connector.RequestFacade@464c9024
+        System.out.println("response ::: " + response); // org.apache.catalina.connector.ResponseFacade@5820cacb
 
+        String userName = request.getParameter("userName");
 
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + userName);
     }
 }
