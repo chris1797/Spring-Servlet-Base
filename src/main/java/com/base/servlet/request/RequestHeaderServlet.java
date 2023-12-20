b9afd120d3a7793e0c4fb9d470d83859cc4a48cc
chris1797 <jaehun6004@gmail.com>
2023-12-21 00:02:53 +0900

    HttpHeaderServlet setup

--- src/main/java/com/base/servlet/request/RequestHeaderServlet.java
+++ src/main/java/com/base/servlet/request/RequestHeaderServlet.java
@@ -1,8 +1,22 @@
 package com.base.servlet.request;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("request.getMethod() ::: " + request.getMethod());
+        System.out.println("request.getProtocol() ::: " + request.getProtocol());
+    }
 }
