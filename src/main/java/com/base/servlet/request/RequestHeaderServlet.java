bb57e1c579612ce36cd27695f7c65faa42ab54cd
chris1797 <jaehun6004@gmail.com>
2023-12-25 15:21:08 +0900

    queryParam, headers

--- src/main/java/com/base/servlet/request/RequestHeaderServlet.java
+++ src/main/java/com/base/servlet/request/RequestHeaderServlet.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,7 +17,35 @@ public class RequestHeaderServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("request.getMethod() ::: " + request.getMethod());
-        System.out.println("request.getProtocol() ::: " + request.getProtocol());
+        printStartLine(request);
+        printCookies(request);
+
+        printLocaleInfo(request);
+    }
+
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println(" ======== REQUEST START ======== ");
+
+        System.out.println("request.getMethod() ::: " + request.getMethod()); // GET
+        System.out.println("request.getProtocol() ::: " + request.getProtocol()); // HTTP/1.1
+        System.out.println("request.getScheme() ::: " + request.getScheme()); // http
+
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println(headerName + " ::: " + headerName));
+    }
+
+    private static void printCookies(HttpServletRequest request) {
+        System.out.println(" ======== Cookies START ======== ");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + " ::: " + cookie.getValue());
+            }
+        }
+    }
+
+    private static void printLocaleInfo(HttpServletRequest request) {
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("Locale ::: " + locale));
+        System.out.println("request.getLocale() ::: " + request.getLocale());
     }
 }
---
+++ src/main/java/com/base/servlet/request/RequestParamServlet.java
@@ -0,0 +1,46 @@
+package com.base.servlet.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+        System.out.println("===================== 전체 파라미터 =====================");
+
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + " ::: " + request.getParameter(paramName)));
+
+
+        System.out.println("===================== 단일 파라미터 =====================");
+        String userName = request.getParameter("userName");
+        String age = request.getParameter("age");
+
+        System.out.println("userName = " + userName);
+        System.out.println("age = " + age);
+
+        System.out.println("===================== 이름이 같은 복수의 파라미터 조회 =====================");
+        /*
+        http://localhost:8080/request-param?userName=tom&age=20&userName=chris로 호출했을 때
+         */
+        List<String> userNames = List.of(request.getParameterValues("userName"));
+//        String[] userNames = request.getParameterValues("userName");
+
+        for (String name : userNames) {
+            System.out.println("userName ::: " + name);
+        }
+        System.out.println("userNames :::" + userNames);
+
+        response.getWriter().write("ok");
+    }
+}
