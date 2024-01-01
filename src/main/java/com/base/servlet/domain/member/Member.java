1d0e02fc1454790fe5dad246c01d34d329e6a145
chris1797 <jaehun6004@gmail.com>
2024-01-01 23:13:46 +0900

    Add Member Test

---
+++ src/main/java/com/base/servlet/domain/member/Member.java
@@ -0,0 +1,16 @@
+package com.base.servlet.domain.member;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/base/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,50 @@
+package com.base.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * product level 에서 ConcurrentHashMap, AtomicLong 고려
+ * 싱글톤으로 구현한 클래스
+ * Spring에서는 싱글톤을 보장해줌
+ */
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+//    MemberRepository 단일 instance
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+//    private 생성자로, 싱글톤을 위해 외부에서 instance를 생성하지 못하도록 하기 위함
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+//        store의 모든 값들로 새로운 ArrayList를 생성해 return
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+//        store 비우기
+        store.clear();
+    }
+
+
+}
