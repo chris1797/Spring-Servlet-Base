7164585b05b3b343db90b25e7b37a4ed475da8bb
chris1797 <jaehun6004@gmail.com>
2024-01-03 01:10:12 +0900

    Add MemberForm setup

--- src/main/java/com/base/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/com/base/servlet/web/servlet/MemberSaveServlet.java
@@ -1,5 +1,6 @@
 package com.base.servlet.web.servlet;
 
+import com.base.servlet.domain.member.Member;
 import com.base.servlet.domain.member.MemberRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,18 +11,23 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
-public class MemberFormServlet extends HttpServlet {
+@WebServlet(name = "memberSaveServlet", urlPatterns = "servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
         response.setContentType("text/html");
         response.setCharacterEncoding("utf-8");
 
-        response.sendRedirect("/basic/form.html");
-
+        response.getWriter().write("ok");
     }
 }
