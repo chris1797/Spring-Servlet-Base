2553a10daa14737446e4eed28bd48c398ef3262b
chris1797 <jaehun6004@gmail.com>
2024-01-04 23:52:28 +0900

    Add member list Servlet

--- src/main/java/com/base/servlet/web/servlet/MemberListServlet.java
+++ src/main/java/com/base/servlet/web/servlet/MemberListServlet.java
@@ -9,21 +9,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
-@WebServlet(name = "memberSaveServlet", urlPatterns = "servlet/members/save")
-public class MemberSaveServlet extends HttpServlet {
+@WebServlet(name = "memberListServlet", urlPatterns = "/servlet/members")
+public class MemberListServlet extends HttpServlet {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
-
-        Member member = new Member(username, age);
-        memberRepository.save(member);
+        List<Member> members = memberRepository.findAll();
 
         response.setContentType("text/html");
         response.setCharacterEncoding("utf-8");
