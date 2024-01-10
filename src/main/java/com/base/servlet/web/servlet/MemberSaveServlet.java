772abc9b3e04745a5db180a6bf496eda77b85608
chris1797 <jaehun6004@gmail.com>
2024-01-11 01:00:41 +0900

    Fix: urlPatterns error

--- src/main/java/com/base/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/com/base/servlet/web/servlet/MemberSaveServlet.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "memberSaveServlet", urlPatterns = "servlet/members/save")
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
 public class MemberSaveServlet extends HttpServlet {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
