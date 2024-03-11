cfc9bb94fb18e63574e68912d1325d88b20d36c4
chris1797 <jaehun6004@gmail.com>
2024-03-11 23:54:33 +0900

    Feat: Add OldController

---
+++ src/main/java/com/base/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package com.base.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+/**
+ * Bean의 이름이 /springmvc/old-controller, URL로 매핑
+ * 이전 버전의 스프링 MVC 컨트롤러
+ * 클라이언트에서 해당 Bean의 이름("/springmvc/old-controller")으로 요청
+ */
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return null;
+    }
+}
