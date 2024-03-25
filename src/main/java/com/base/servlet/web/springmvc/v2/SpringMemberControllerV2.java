167384ad8aacf38e74ed34f81b02de31c8f59248
chris1797 <jaehun6004@gmail.com>
2024-03-25 23:37:03 +0900

    Feat: Controller RequestMapping

--- src/main/java/com/base/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/com/base/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,4 +1,4 @@
-package com.base.servlet.web.springmvc.v1;
+package com.base.servlet.web.springmvc.v2;
 
 import com.base.servlet.domain.member.Member;
 import com.base.servlet.domain.member.MemberRepository;
@@ -8,19 +8,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
-public class SpringMemberSaveControllerV1 {
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    // response를 사용하지는 않지만 다음과 같이 받을 수 있다는 것을 참고
-    @RequestMapping("/springmvc/v1/members/save")
-    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
-         Member member = new Member(username, age);
-         memberRepository.save(member);
+        Member member = new Member(username, age);
+        memberRepository.save(member);
 
         ModelAndView mv = new ModelAndView("save-result");
         mv.addObject("member", member);
