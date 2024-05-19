b11f6eb021c78f5c5a3e61c1a7deffd129e039a1
chris1797 <jaehun6004@gmail.com>
2024-05-20 00:28:17 +0900

    Add RequestBodyJsonController

---
+++ src/main/java/com/base/servlet/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,32 @@
+package com.base.servlet.basic.request;
+
+import com.base.servlet.basic.HelloData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody = {}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}, age={}", helloData.getUserName(), helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+}
