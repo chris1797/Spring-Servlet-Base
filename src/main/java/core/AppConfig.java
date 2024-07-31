8f6a33b35006e6d93afbce70a86abc51e0765102
chris1797 <jaehun6004@gmail.com>
2024-08-01 00:46:18 +0900

    AppConfig refactoring

--- src/main/java/core/AppConfig.java
+++ src/main/java/core/AppConfig.java
@@ -1,6 +1,8 @@
 package core;
 
+import core.discount.DiscountPolicy;
 import core.discount.FixDiscountPolicy;
+import core.discount.RateDiscountPolicy;
 import core.member.MemberService;
 import core.member.MemberServiceImpl;
 import core.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
     }
 }
