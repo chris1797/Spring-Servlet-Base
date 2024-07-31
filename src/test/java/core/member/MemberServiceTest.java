dd006f89c14bb8171519b87da51d9eb290f60f04
chris1797 <jaehun6004@gmail.com>
2024-07-31 23:09:10 +0900

    Add AppConfig

--- src/test/java/core/member/MemberServiceTest.java
+++ src/test/java/core/member/MemberServiceTest.java
@@ -1,13 +1,22 @@
 package core.member;
 
+import core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
     @Test
     void join() {
         // given
-        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
 
         // when
--- src/test/java/core/order/OrderServiceTest.java
+++ src/test/java/core/order/OrderServiceTest.java
@@ -1,17 +1,26 @@
 package core.order;
 
+import core.AppConfig;
 import core.member.Grade;
 import core.member.Member;
 import core.member.MemberService;
 import core.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
 
     @Test
