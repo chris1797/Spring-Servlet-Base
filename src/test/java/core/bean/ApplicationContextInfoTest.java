ebbc1c93b6227584611ee3d96d687a3fa4a71899
chris1797 <jaehun6004@gmail.com>
2024-08-05 00:17:14 +0900

    Add test of ApplicationContext

--- src/test/java/core/bean/ApplicationContextInfoTest.java
+++ src/test/java/core/bean/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package core.bean;
 import core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextInfoTest {
@@ -12,10 +13,31 @@ public class ApplicationContextInfoTest {
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean() {
+        // ac.getBeanDefinitionNames() : 스프링에 등록된 모든 빈 이름을 조회
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+
+            // ac.getBean() : 빈 이름으로 빈 객체(인스턴스)를 조회
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+
+        }
+    }
+
+    @Test
+    @DisplayName("직접 등록한 빈 출력하기")
+    void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            // BeanDefinition : 빈에 대한 정보(설정 메타 정보)를 제공
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // BeanDefinition.ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            // BeanDefinition.ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
 
         }
     }
