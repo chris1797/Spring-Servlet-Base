4e832b110b568385c1e38c1118b745890c8a12d9
chris1797 <jaehun6004@gmail.com>
2024-08-17 21:54:19 +0900

    @ComponentScan filter test

--- src/test/java/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -16,17 +16,22 @@ public class ComponentFilterAppConfigTest {
     void filterScan() {
         var ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
         for (var name : ac.getBeanDefinitionNames()) {
+            // beanA가 등록되어 있음을 확인할 수 있다. :: name = beanA
             System.out.println("name = " + name);
         }
 
         BeanA beanA = ac.getBean(BeanA.class);
+
+        // BeanA는 include 대상이므로 스프링 빈에 등록된다.
         assertThat(beanA).isNotNull();
 
+        // BeanB는 include 대상에서 제외되었으므로 스프링 빈에 등록되지 않는다.
         assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean(BeanB.class));
     }
 
     @Configuration
     @ComponentScan(
+            // type = FilterType.ANNOTATION 기본값
             includeFilters = @ComponentScan.Filter(MyIncludeComponent.class),
             excludeFilters = @ComponentScan.Filter(MyExcludeComponent.class)
     )
