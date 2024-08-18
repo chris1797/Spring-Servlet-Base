c8fc0ce0a5b18658bd5f67cab74b821d99ece99b
chris1797 <jaehun6004@gmail.com>
2024-08-18 23:00:02 +0900

    DI

--- src/main/java/core/order/OrderServiceImpl.java
+++ src/main/java/core/order/OrderServiceImpl.java
@@ -9,16 +9,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
+    /*
+    필드 주입이 권장되지 않는 이유
+    - 변경이 불가능하다.
+    - 필요한 빈들과의 의존관계 주입이 불가능하기 때문에 테스트 코드 작성이 어렵다.
+     */
 
-    // Test 용도 Getter
-    @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    /**
+     * 생성자 주입의 의존관계 주입 시점은 해당 빈의 생성 시점(생성자 호출 시점)에 스프링이 알아서 주입해준다.
+     */
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
+    }
+
+    /**
+     * <p>수정자 주입의 의존관계 주입 시점은 해당 빈의 생성 시점 이후, 스프링 컨테이너가 관리중인 빈이 모두 생성되고, 의존관계 주입이 완료된 이후에 호출된다.</p>
+     * 수정자 주입은 선택, 변경 가능성이 있는 의존관계에 사용한다.
+     */
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
     }
 
 
