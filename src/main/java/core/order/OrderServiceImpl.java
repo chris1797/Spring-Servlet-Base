52011dab8c93a55136415d1f874a9afa0b37d3f9
chris1797 <jaehun6004@gmail.com>
2024-08-21 23:22:59 +0900

    Add lombok

--- src/main/java/core/order/OrderServiceImpl.java
+++ src/main/java/core/order/OrderServiceImpl.java
@@ -4,17 +4,18 @@ import core.discount.DiscountPolicy;
 import core.member.Member;
 import core.member.MemberRepository;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     /*
     필드 주입이 권장되지 않는 이유
     - 변경이 불가능하다.
     - 필요한 빈들과의 의존관계 주입이 불가능하기 때문에 테스트 코드 작성이 어렵다.
      */
-
 
     @Getter
     private final MemberRepository memberRepository;
@@ -23,11 +24,10 @@ public class OrderServiceImpl implements OrderService {
     /**
      * 생성자 주입의 의존관계 주입 시점은 해당 빈의 생성 시점(생성자 호출 시점)에 스프링이 알아서 주입해준다.
      */
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     /**
--- src/test/java/core/order/OrderServiceImplTest.java
+++ src/test/java/core/order/OrderServiceImplTest.java
@@ -7,7 +7,6 @@ import core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
 
@@ -20,7 +19,6 @@ class OrderServiceImplTest {
         Order order = orderService.createOrder(1L, "itemA", 10000);
 
         assertThat(order.getDiscountPrice()).isEqualTo(1000);
-
     }
 
 }
