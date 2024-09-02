ee2b2705ab627b37a403a6392ff43297f079ff51
chris1797 <jaehun6004@gmail.com>
2024-09-03 00:07:25 +0900

    MyLogger(request scope)에 Provider 적용

--- src/main/java/core/common/MyLogger.java
+++ src/main/java/core/common/MyLogger.java
@@ -7,6 +7,11 @@ import lombok.Setter;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+/**
+ * <h3>Request Scope bean</h3>
+ * <p>LogDemoController, LogDemoService 에서 각각 다른 MyLogger 가 생성되어도 같은 HTTP 요청이면 같은 MyLogger 를 참조하도록 하기 위해 request scope 로 설정했다.</p>
+ * MyLogger 는 HTTP 요청 당 하나씩 생성되고, HTTP 요청이 끝나는 시점에 소멸한다.
+ */
 @Setter
 @Component
 @Scope(value = "request")
@@ -23,11 +28,11 @@ public class MyLogger {
     @PostConstruct
     public void init() {
         this.uuid = java.util.UUID.randomUUID().toString();
-        System.out.println("[" + uuid + "][" + requestURL + "] request scope bean create:" + this);
+        System.out.println("[" + uuid + "][" + requestURL + "] request scope bean create: " + this);
     }
 
     @PreDestroy
     public void close() {
-        System.out.println("[" + uuid + "][" + requestURL + "] close:" + this);
+        System.out.println("[" + uuid + "][" + requestURL + "] close: " + this);
     }
 }
--- src/main/java/core/web/LogDemoController.java
+++ src/main/java/core/web/LogDemoController.java
@@ -11,16 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LogDemoController {
 
+    /**
+     * <h3>ObjectProvider</h3>
+     * <p>ObjectProvider 는 지정한 빈을 컨테이너에서 대신 찾아주는 DL(dependency lookup) 빈이다.</p>
+     * <p>MyLogger 는 request scope 이므로 HTTP 요청이 들어올 때마다 새로운 MyLogger 가 생성되어야 하는데,</p>
+     * <p>ObjectProvider 없이 MyLogger가 생성되는 시점에 주입받으면, MyLogger 는 싱글톤 빈이므로 주입받은 시점의 MyLogger 를 계속 사용하게 된다.</p>
+     * <p>ObjectProvider 를 사용하면 개발자가 원하는, getObject() 를 호출하는 시점에 컨테이너에서 MyLogger 를 찾아서 반환한다.</p>
+     *
+     *
+     */
     private final ObjectProvider<MyLogger> myLoggerProvider;
     private final LogDemoService logDemoService;
 
     @RequestMapping("log-demo")
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
         MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
